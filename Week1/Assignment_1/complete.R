35454e7e223c900f26c15cf2557ca326ce372bec
Mazard13 <sharmad.navelker@gmail.com>
2014-10-10 03:23:29 +0530

    corrected order issues by interchanging for loops(file and id) and if(i) conditions. Receiving correct ouptut - program is slow

--- Week1/Assignment_1/complete.R
+++ Week1/Assignment_1/complete.R
@@ -6,26 +6,25 @@ complete <- function(directory, id = 1:332){
   
   i <- 1
   
-  for (file in file_list){
+  for (y in id){ #for outside gives preference to order
+    for (file in file_list){ 
     temp_data <- read.csv(file)
-    
-    for (y in id){ #taking all id IN INCREASING ORDER!(warning)
-      
-      if (temp_data[2,4] == y){
+        
+      if (temp_data[2,4] == y){   # 2 is random . can put any value in the range of the column
         clean_data <- temp_data[complete.cases(temp_data),] #removing all NAs
         wanted_data <- c(y,sum(clean_data[,4]== y ))  # creating vectors of id and sum(id)
     
-        if (i==1){
-          dataset <- wanted_data
-          i<-i+1
-        }  
         if(i>1){
           dataset<-rbind(dataset,wanted_data)
         }
+        if (i==1){#if this condition was placed earlier , output would have the 2  initial rows as same
+          dataset <- wanted_data
+          i<-i+1
+        }        
       }
     }
   }
-  frame<-data.frame(id = dataset[,1],nobs = dataset[,2])
+  frame<-data.frame(id = dataset[,1],nobs = dataset[,2]) #defined data frames with column names
   
  frame
 }
