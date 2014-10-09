92a92ff0576ee326ce9be1bbcf6e91204c7b067e
Mazard13 <sharmad.navelker@gmail.com>
2014-10-09 21:32:37 +0530

    Made changes to complete cases. Removes NA only from pollutant column. The final code

--- Week1/Assignment_1/pollutantmean.R
+++ Week1/Assignment_1/pollutantmean.R
@@ -6,13 +6,21 @@ pollutantmean <- function(directory, pollutant , id = 1:332){
     
   i<-1
   
+  #bringing in pollutants
+  if (pollutant == "sulfate"){
+    x <- 2 
+  }
+  if (pollutant == "nitrate"){
+    x <- 3
+  }
+  
   #merging files into dataset
   for(file in file_list){ 
     
     #temporarily storing files
     temp_data <- read.csv(file)
     
-    clean_data <- temp_data[complete.cases(temp_data),]
+    clean_data <- temp_data[complete.cases(temp_data[,x]),] # deletes NA from column x
     
     if(i==1){
       dataset<-clean_data
@@ -23,20 +31,12 @@ pollutantmean <- function(directory, pollutant , id = 1:332){
   i<-i+1
   }
   #dataset created - complete file with all IDs not containing NA values
-  
-  #bringin in pollutants
-  if (pollutant == "sulfate"){
-    x <- 2 
-  }
-  if (pollutant == "nitrate"){
-    x <- 3
-  }
-  
+    
   #Using ID
   id_min<-min(id)
   id_max<-max(id)
   data_ID <- subset(dataset,dataset$ID >= id_min & dataset$ID <= id_max)
   
-  mean(data_ID[,x])
+  mean(data_ID[,x]) # gives mean in column x
 }
 
